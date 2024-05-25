971ed438b40749d327c6d42082b75bdaf1d10977
mongkonwan-cnt <mongkonwan.num@gmail.com>
2024-05-25 18:50:35 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -37,6 +38,10 @@ export default function Home() {
           height={37}
           priority
         />
+      </div>
+
+      <div>
+       <Button> Im in shadcn UI </Button>   
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
